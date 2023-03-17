084ae36a65d1109805666b04db7632c5c54188c2
Hai Vu <namhaivu97@gmail.com>
2023-03-17 05:09:49 -0400

    local.r updates2

--- local.r
+++ local.r
@@ -2,4 +2,4 @@
 # Deploy RShiny app from GitHub
 install.packages("shiny")
 library(shiny)
-runGitHub(repo = "TreeCensus_Dashboard", username = "namhaivu173", ref = "main")
+shiny::runGitHub(repo = "TreeCensus_Dashboard", username = "namhaivu173", ref = "main")
